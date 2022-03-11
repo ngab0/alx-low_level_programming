3517ee7335c89e49ed3bddaecc23bb8d7e940ad9
ngab0 <jackdonnily@gmail.com>
2022-03-11 22:48:07 +0200

    combination of single digits in base10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,16 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i < 57)
+		{
 			putchar(44);
 			putchar(32);
+		}
 	}
 	putchar(10);
 	return (0);
