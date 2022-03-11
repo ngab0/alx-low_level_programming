1c30f5a51b2b9df83bdfdb8396bb886d81544fca
ngab0 <jackdonnily@gmail.com>
2022-03-11 21:48:20 +0200

    Single digit numbers in base10 (using putchar)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print single digit numbers in base 10
+* Description : you can only use putchar function twice
+* Return: 0
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	do {
+		putchar(i);
+		i++;
+	} while (i < 58);
+	printf("\n");
+	return (0);
+}
