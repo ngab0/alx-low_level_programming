702b1d4437b449ff3de3084ae7c8ecf7554194d0
ngab0 <jackdonnily@gmail.com>
2022-03-11 21:16:04 +0200

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabet
+* Description : use the putchar function
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(i + 97);
+	}
+	printf("\n");
+	return (0);
+}
