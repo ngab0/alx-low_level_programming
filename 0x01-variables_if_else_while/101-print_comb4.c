276d3a0218bedbdd8732291ea9b8801e285211f1
ngab0 <jackdonnily@gmail.com>
2022-03-12 02:49:39 +0200

    All possible combinations of 3digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,20 +10,24 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 48; i < 56; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = 49; j < 57; j++)
+		for (j = 48; j < 58; j++)
 		{
-			for (k = 50; k < 58; k++)
+			for (k = 48; k < 58; k++)
 			{
-				if (i != j || i != k || j != k)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-					putchar(44);
-					putchar(32);
-				}
+				if (i != j && i != k && j != k)
+					if (i < j && j < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						if (i != 55)
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
 			}
 		}
 	}
