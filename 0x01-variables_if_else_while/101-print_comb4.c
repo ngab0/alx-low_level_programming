73748d382a64aafa02601e36022e2e30e634ede4
ngab0 <jackdonnily@gmail.com>
2022-03-12 04:48:20 +0200

    All possible combinations of 3digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - print all combinations of single digits in base10
+* main - print all combinations of double digits in base10
 * Description : use the putchar function
 * Return: 0
 */
@@ -33,3 +33,21 @@ int main(void)
 	putchar(10);
 	return (0);
 }
+
+int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(32);
+			putchar(k);
+			putchar(l);
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
