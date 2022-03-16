d0fea9cb53a885d9c05c7245cdfac3a0e33b359d
ngab0 <jackdonnily@gmail.com>
2022-03-16 11:04:23 +0200

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - prints '_putchar'
+* Description : Uses _putchar function
+* Return: 0
+*/
+
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar(10);
+	return (0);
+}
