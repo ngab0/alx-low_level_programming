cecab87bf07a753dbb4fd462958655bd7a01a296
ngab0 <jackdonnily@gmail.com>
2022-03-17 07:35:43 +0200

    Last jaunt

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,17 +14,20 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0))
 	{
-		if (month > 1 && day > 59)
+		if (month == 2 && day < 61)
 		{
 			day++;
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
 		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		else
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if (month == 2 && day > 59)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
