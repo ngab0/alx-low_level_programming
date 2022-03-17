351f85fc570784d8a300169cb055f1831496df3e
ngab0 <jackdonnily@gmail.com>
2022-03-17 05:13:41 +0200

    Print largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,11 @@ int largest_number(int a, int b, int c)
 	int largest = 0;
 	/*printf("%d\n", largest);*/
 
-	if (a > b && a > c)
+	if ((a > b && a > c) || (a == b && a > c) || (a == c && a > b))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b > a && b > c) || (b == c && b > a))
 	{
 		largest = b;
 	}
