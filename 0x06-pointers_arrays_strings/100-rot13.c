9be7e2069e981c8237a7ec3bef8c4c7b3bc99304
ngab0 <jackdonnily@gmail.com>
2022-03-25 05:30:58 +0200

    Encode Strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,9 +8,10 @@
 
 char *rot13(char *s)
 {
-	int i = 0; j = 0;
+	int i = 0; 
+        int j = 0;
 	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
-	char string_alpha = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 	while (s[i] != '\0')
 	{
