3fe9605c3bced1300e6c17b56484b8cfe7e46dea
ngab0 <jackdonnily@gmail.com>
2022-03-28 17:03:04 +0000

    Fill memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ *_memset- fill in bytes of a memory area with a constant byte
+ *@s: pointer to memory area
+ *@b: constant byte to be filled in
+ *@n: number of times b will be filled in
+ *Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	char *a = s;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (a);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Even more pointers, arrays and strngs. 
+Double pointers
+2-dimensional arrays
+
