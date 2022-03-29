c453bff78386d5432889d641449455340f18f4ed
ngab0 <jackdonnily@gmail.com>
2022-03-29 00:25:15 +0000

    copy memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,22 @@
 /*#include <stdio.h>*/
 #include "main.h"
 
-/*
- * _memcpy - copy n bytes from one area to another
- * @dest: place to be copied to
- * @src: place to be copied from
- * @n: number of bytes to copied
+/**
+ *_memcpy- copy n bytes from one area to another
+ *@dest: place to be copied to
+ *@src: place to be copied from
+ *@n: number of bytes to copied
+ *Return: pointer to memory of dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
 	unsigned int i = 0;
 	char *a = dest;
 
 	while (i < n)
 	{
-		*(dest + i) = _putchar(*(src + i));
+		dest[i] = _putchar(src[i]);
 		i++;
 	}
 	return (a);
