f85fc4ee1d3aeab481e95031110dda23f71b8385
ngab0 <jackdonnily@gmail.com>
2022-03-29 22:54:20 +0000

    Print the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  *print_chessboard- print a 2d array chessboard
- *@a: 2d array of characters
+ *@(a: 2d array of characters
  *Return: void
  */
 
@@ -16,10 +16,10 @@ void print_chessboard(char *(a)[8])
 		j = 0;
 		while (j < 8)
 		{
-			_putchar(a[i][j]);
+			_putchar (a[i][j]);
 			j++;
 		}
-		_putchar('\n');
+		_putchar ('\n');
 		i++;
 	}
 }
