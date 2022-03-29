9558cb37069a033f3c0892179a6ddd129093df08
ngab0 <jackdonnily@gmail.com>
2022-03-29 22:11:52 +0000

    locate characters in a string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strspn- get the length of a prefix substring
+ *@s: String to be scanned
+ *@accept: String containing list of characters to be matched
+ *Return: count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count = 0;
+	char *str1, *str2;
+
+	str1 = s;
+	str2 = accept;
+
+	for (i = 0; str1[i] != '\0'; i++)
+	{
+		for (j = 0; str2[j] != '\0'; j++)
+		{
+			if (str2[j] == str1[i])
+				count++;
+				break;
+		}
+		if (s[i] != accept[j])
+			break;
+	}
+	return (count);
+}
