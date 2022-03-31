c08e7373c11bdf2dcfe2fac9a95cf6e407e96451
ngab0 <jackdonnily@gmail.com>
2022-03-31 03:01:11 +0000

    Wild comparison

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,10 +12,12 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
+		{
 			return (wildcmp(s1, s2 + 1));
+		}
 		return (*s2 == '\0');
 	}
-	else if (*s2 == '*')
+	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wilcmp(s1, s2 + 1));
 	else if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
