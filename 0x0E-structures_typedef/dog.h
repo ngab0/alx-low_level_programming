1cbcb50da31af91c9ac6f87b1d6e3fac15b6f0b8
ngab0 <jackdonnily@gmail.com>
2022-04-12 23:39:31 +0200

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,17 @@
  * @owner: owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
-};
+}dog_t;
 
-typedef struct dog imbwa;
+/*typedef struct dog dog_t;*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
