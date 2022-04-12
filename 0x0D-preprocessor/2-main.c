ddfcdfcb134eb08eeecea2cf681b9878b29684ac
ngab0 <jackdonnily@gmail.com>
2022-04-12 22:07:05 +0200

    name of file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: prints the name of the file it was
+ * compiled from
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
