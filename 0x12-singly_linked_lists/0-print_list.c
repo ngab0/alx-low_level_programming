8c42e98a717b147ad4c4118b02a04bbce4eec3b5
ngab0 <jackdonnily@gmail.com>
2022-04-22 00:31:08 +0200

    Print elements of the list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_list- Print all the elements of a list
+ * @h: pointer to the linked list
+ * Return: number of nodes of the linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (
+	{
+
