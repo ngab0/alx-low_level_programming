b97274451c233666e28f3e8d82e6a6742178c4f3
ngab0 <jackdonnily@gmail.com>
2022-04-22 03:09:19 +0200

    Add nodes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node- adds a new node to beginning of the list
+ * @head: pointer to head
+ * @str: string to add
+ * Return: address if true, NULL if fail
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int i;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; str[i];)
+		i++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node_end- add a new node at the end of a list
+ * @head: pointer to head of list
+ * @str: string to be duplicated
+ * Return: address of new element or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *current;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	s = strdup(str);
+	if (s == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL)
+	new->str = s;
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
+	return (*head);
+}
