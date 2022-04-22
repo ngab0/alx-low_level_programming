d4571df429c5d62b31be9411289c39b4c2c66d0d
ngab0 <jackdonnily@gmail.com>
2022-04-22 04:33:02 +0200

    Linked Lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,8 @@
 /**
  * print_text - prints text
  */
-void print_text(void)
+
+void __attribute__ ((constructor))print_text(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
