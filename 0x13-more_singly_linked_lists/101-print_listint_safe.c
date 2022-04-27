a9812aad375f4ec355aad7f2657716f165ea3b3d
ngab0 <jackdonnily@gmail.com>
2022-04-27 20:21:06 +0200

    12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,15 +18,16 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)head, head->n);
 
 		if (diff > 0)
+		{
 			head = head->next;
+		}
 
 		else
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)head->next, head->n->next);
 			break;
 		}
 	}
 
 	return (num);
 }
-
