c315ee26a4454de090a4005842580b7279091e9d
ngab0 <jackdonnily@gmail.com>
2022-04-27 16:34:02 +0200

    print elements of list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint- prints all elements of linked list
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 1;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
