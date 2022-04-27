f2c7417602735f414d1905c4228c938c44bb8826
ngab0 <jackdonnily@gmail.com>
2022-04-27 17:11:03 +0200

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2- free a list
+ * @head: pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	while ((*head)->next != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	free(*head);
+	*head = NULL;
+}
